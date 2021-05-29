d727e8b0ee42d026bac8df78e2cd080de8731e88
OKT <rudxor97@naver.com>
2021-05-29 23:34:33 +0900

    children 으로 객체를 받아왔을 경우 처리

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <template>
   <v-app>
-    <template v-for="(item, idx) in items">
-      <component
-        :is="item.type"
-        :key="idx"
-        v-bind="{ ...item.props }"
-      ></component>
-    </template>
+    <v-main>
+      <template v-for="(item, idx) in items">
+        <v-row :key="`row-${idx}`">
+          <template v-if="item.children">
+            <template v-for="(child, childIdx) in item.children">
+              <v-col :cols="child.cols" :key="`child-${childIdx}`">
+                <component
+                  :is="child.type"
+                  :key="`child-item-${childIdx}`"
+                  v-bind="{ ...child.props }"
+                ></component>
+              </v-col>
+            </template>
+          </template>
+          <template v-else>
+            <v-col>
+              <component
+                :is="item.type"
+                :key="`item-${idx}`"
+                v-bind="{ ...item.props }"
+              ></component>
+            </v-col>
+          </template>
+        </v-row>
+      </template>
+    </v-main>
   </v-app>
 </template>
 
@@ -21,7 +40,20 @@ import { JsonToFormProps } from "./plugins/json-to-form";
 export default class App extends Vue {
   items: JsonToFormProps[] = [
     {
-      type: "v-file",
+      children: [
+        {
+          type: "v-text",
+          cols: 3,
+        },
+        {
+          type: "v-text",
+          cols: 3,
+        },
+        {
+          type: "v-text",
+          cols: 3,
+        },
+      ],
     },
     {
       type: "v-text",
--- src/plugins/json-to-form.ts
+++ src/plugins/json-to-form.ts
@@ -1,40 +1,31 @@
 import Vue from "vue";
-import {
-  VAutocomplete,
-  VCol,
-  VFileInput,
-  VRow,
-  VTextarea,
-  VTextField,
-} from "vuetify/lib";
+import { VAutocomplete, VFileInput, VTextarea, VTextField } from "vuetify/lib";
 
 export const V_FILE = "v-file";
 export const V_TEXT = "v-text";
 export const V_AUTOCOMPLETE = "v-autocomplete";
 export const V_TEXTAREA = "v-textarea";
-export const V_ROW = "v-row";
-export const V_COL = "v-col";
 
 export type JsonToInputTypes =
   | typeof V_FILE
   | typeof V_TEXT
   | typeof V_AUTOCOMPLETE
-  | typeof V_TEXTAREA
-  | typeof V_ROW
-  | typeof V_COL;
+  | typeof V_TEXTAREA;
 
 export type JsonToFormProps = {
-  type: JsonToInputTypes;
+  type?: JsonToInputTypes;
   props?: unknown;
-  children?: JsonToFormProps[];
+  children?: JsonToFormChildrenProps[];
+};
+
+export type JsonToFormChildrenProps = JsonToFormProps & {
+  cols: number;
 };
 
 Vue.component(V_FILE, VFileInput);
 Vue.component(V_TEXT, VTextField);
 Vue.component(V_AUTOCOMPLETE, VAutocomplete);
 Vue.component(V_TEXTAREA, VTextarea);
-Vue.component(V_ROW, VRow);
-Vue.component(V_COL, VCol);
 
 // const prototype = Vue.prototype;
 
